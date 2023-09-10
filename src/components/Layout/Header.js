23c799054d7e0b8617de78b7f9e8422070e078a5
FTMFR <fatemefarajzade018@gmail.com>
2023-09-10 18:44:15 +0330

    add cart component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,6 +1,7 @@
 import { Fragment } from "react";
+import HeaderCartButton from "./HeaderCartButton";
 import mealsImg from '../../assets/meals.jpg';
-import styles from './Header.module.css'
+import styles from './Header.module.css';
 
 
 const Header = (props) => {
@@ -8,7 +9,7 @@ const Header = (props) => {
         <Fragment>
             <header className={styles.header}>
                 <h1>React Meals</h1>
-                <button>Cart</button>
+                <HeaderCartButton />
             </header>
             <div className={styles['main-image']}>
                 <img src={mealsImg} alt="A table full of delicipus food!" />
