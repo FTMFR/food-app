772c3db8128179e6e4f3056db435684ae88b4f09
FTMFR <fatemefarajzade018@gmail.com>
2023-09-10 20:12:56 +0330

    add modal component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Fragment } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
 
 
 const App = () => {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
