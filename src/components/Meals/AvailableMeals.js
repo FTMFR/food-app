2df741846a42c86915b727d02c5636d47f03b066
FTMFR <fatemefarajzade018@gmail.com>
2023-09-13 23:05:16 +0330

    add error handle

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,29 +6,51 @@ import styles from './Available.module.css';
 
 const AvailableMeals = () => {
     const [meals, setMeals] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState(null);
 
     useEffect(() => {
         const fetchMeals = async () => {
             const response =
                 await fetch(
                     'https://food-app-api-a509f-default-rtdb.firebaseio.com/DUMMY_MEALS.json');
+
+            if (!response.ok) {
+                throw new Error('Something went wrong!');
+            };
+
             const data = await response.json();
 
             const loadedMeals = [];
 
             for (const key in data) {
                 loadedMeals.push({
-                    id: key,
+                    id: data[key].id,
                     name: data[key].name,
                     description: data[key].description,
                     price: data[key].price
                 });
             };
             setMeals(loadedMeals);
+            setIsLoading(false);
         };
-        fetchMeals();
-    }), [];
+        fetchMeals().catch(error => {
+            setIsLoading(false);
+            setHttpError(error.message)
+        });
+    }, []);
 
+    if (isLoading) {
+        return <section className={styles.mealIsLoading}>
+            <p>Loading...</p>
+        </section>
+    };
+
+    if (httpError) {
+        return <section className={styles.mealsError}>
+            <p>{httpError}</p>
+        </section>
+    };
 
     const mealsList = meals.map((meal) =>
         <MealItem
